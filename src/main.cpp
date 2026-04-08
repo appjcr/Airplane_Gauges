38fcd7ddf4c5f0cfde0588f90a129cc9a3178705
appjcr <appjcr@gmail.com>
2026-04-07 22:44:06 -0500

    Added capatance fuel sensors commented out resistive fuel sensors

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
    You also need to copy 'lvgl/examples' to 'lvgl/src/examples'. Similarly for the demos 'lvgl/demos' to 'lvgl/src/demos'.
 */
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 #include <Wire.h>
 #include <Adafruit_ADS7830.h>
 Adafruit_ADS7830 ad7830;
@@ -26,17 +27,29 @@ bool first_time = true;
 #define I2C_SDA 18
 #define I2C_SCL 17
 
-const int sensorPin = 6; // Pin with Interrupt for flow meter
+// Pin 5 as RX Left tank, Pin 6 as RX Right tank, Pin -1 as TX (not used)
+SoftwareSerial mySerial_L(5, -1); 
+SoftwareSerial mySerial_R(6, -1); 
+
+const int sensorPin = 7; // Pin with Interrupt for flow meter
 volatile long pulseCount = 0;
 float flowRate = 0.0;
 unsigned int flowMilliLitres = 0;
 unsigned long totalMilliLitres = 0;
 unsigned long oldTime = 0;
 
+int32_t L_counter = 0;
+int32_t bytesRead_L = 0;
+uint8_t buffer_L[10] = {0,0,0,0,0,0,0,0,0,0}; // Adjust size based on expected line length
+int32_t R_counter = 0;
+int32_t bytesRead_R = 0;
+uint8_t buffer_R[10] = {0,0,0,0,0,0,0,0,0,0}; // Adjust size based on expected line length
 uint8_t value[7];
 
-int16_t Empty_fuel_resistance_value = 67;
-int16_t Full_fuel_resistance_value = 13;
+int16_t Empty_fuel_L_capacitance_value = 1340;
+int16_t Full_fuel_L_capacitance_value = 905;
+int16_t Empty_fuel_R_capacitance_value = 1340;
+int16_t Full_fuel_R_capacitance_value = 905;
 int16_t Empty_flaps_resistance_value = 67;
 int16_t Full_flaps_resistance_value = 13;
 int16_t Empty_elev_resistance_value = 67;
@@ -131,47 +144,231 @@ int32_t smooth_fuel_readings_R (int32_t read_fuel_value_R ) {
     return(averageFuel_R);
 }
 
-static void sensors_timer_cb(lv_timer_t * timer1)
+static void fuel_sensors_timer_cb(lv_timer_t * timer1)
 {
     LV_UNUSED(timer1);
+    // read ttl to serial sensors Left tank
+    if (mySerial_L.available() >= 2) {
+        L_counter++;
+        memset(buffer_L, 0, sizeof(buffer_L));
+        // Read the line until newline character or buffer filled
+        bytesRead_L = mySerial_L.readBytesUntil('\n', buffer_L, sizeof(buffer_L));
+        if (bytesRead_L >= 2) {
+            // (Little Endian)
+            L_counter = (buffer_L[0] | buffer_L[1] << 8);
+
+            if(L_counter < Full_fuel_L_capacitance_value) L_counter = Full_fuel_L_capacitance_value;
+            if(L_counter > Empty_fuel_L_capacitance_value) L_counter = Empty_fuel_L_capacitance_value;
+            Serial.print("Left counter: ");
+            Serial.print(L_counter);
+            Serial.print(" bytesRead: ");
+            Serial.print(bytesRead_L);
+            Serial.print(" buffer: ");
+            int i = 0;
+            // Loop as long as the current character is not 0 AND we are within array bounds
+            while (i < 10 && buffer_L[i] != 0) {
+                Serial.print(buffer_L[i], HEX); // Print the character
+                Serial.print(" ");
+                i++;
+            }
+            Serial.print(" Raw Bytes: ");
+            Serial.print(buffer_L[0], HEX);
+            Serial.print(" ");
+            Serial.print(buffer_L[1], HEX);
+            Serial.print(" Decoded 16-bit Counter: ");
+            Serial.println(L_counter);
+
+            Fuel_L_value = smooth_fuel_readings_L(L_counter);
+            Serial.printf("Raw smoothed sensor L value: %" PRId32 "\n", Fuel_L_value);
+            // Greater than or equal to 1320 is considered empty, so set to 0% 
+            if (Fuel_L_value >= 1320) Fuel_L_value = 0;
+            if (Fuel_L_value < 1320) Fuel_L_value = 2; 
+            if (Fuel_L_value < 1300) Fuel_L_value = 4; 
+            if (Fuel_L_value < 1290) Fuel_L_value = 6; 
+            if (Fuel_L_value < 1278) Fuel_L_value = 8; 
+            if (Fuel_L_value < 1268) Fuel_L_value = 10; 
+            if (Fuel_L_value < 1258) Fuel_L_value = 12; 
+            if (Fuel_L_value < 1248) Fuel_L_value = 14; 
+            if (Fuel_L_value < 1239) Fuel_L_value = 16; 
+            if (Fuel_L_value < 1229) Fuel_L_value = 18; 
+            if (Fuel_L_value < 1219) Fuel_L_value = 20; 
+            if (Fuel_L_value < 1209) Fuel_L_value = 22; 
+            if (Fuel_L_value < 1198) Fuel_L_value = 24; 
+            if (Fuel_L_value < 1191) Fuel_L_value = 26; 
+            if (Fuel_L_value < 1185) Fuel_L_value = 28; 
+            if (Fuel_L_value < 1178) Fuel_L_value = 30; 
+            if (Fuel_L_value < 1172) Fuel_L_value = 33; 
+            if (Fuel_L_value < 1163) Fuel_L_value = 35; 
+            if (Fuel_L_value < 1154) Fuel_L_value = 37; 
+            if (Fuel_L_value < 1145) Fuel_L_value = 39; 
+            if (Fuel_L_value < 1137) Fuel_L_value = 42; 
+            if (Fuel_L_value < 1130) Fuel_L_value = 44; 
+            if (Fuel_L_value < 1122) Fuel_L_value = 46; 
+            if (Fuel_L_value < 1115) Fuel_L_value = 48; 
+            if (Fuel_L_value < 1107) Fuel_L_value = 50; 
+            if (Fuel_L_value < 1099) Fuel_L_value = 52; 
+            if (Fuel_L_value < 1090) Fuel_L_value = 54; 
+            if (Fuel_L_value < 1082) Fuel_L_value = 56; 
+            if (Fuel_L_value < 1073) Fuel_L_value = 58; 
+            if (Fuel_L_value < 1064) Fuel_L_value = 60; 
+            if (Fuel_L_value < 1056) Fuel_L_value = 62; 
+            if (Fuel_L_value < 1048) Fuel_L_value = 64; 
+            if (Fuel_L_value < 1040) Fuel_L_value = 66; 
+            if (Fuel_L_value < 1031) Fuel_L_value = 68; 
+            if (Fuel_L_value < 1022) Fuel_L_value = 70; 
+            if (Fuel_L_value < 1013) Fuel_L_value = 72; 
+            if (Fuel_L_value < 1005) Fuel_L_value = 74; 
+            if (Fuel_L_value < 995) Fuel_L_value = 76; 
+            if (Fuel_L_value < 985) Fuel_L_value = 79; 
+            if (Fuel_L_value < 975) Fuel_L_value = 81; 
+            if (Fuel_L_value < 965) Fuel_L_value = 83; 
+            if (Fuel_L_value < 952) Fuel_L_value = 85; 
+            if (Fuel_L_value < 943) Fuel_L_value = 87; 
+            if (Fuel_L_value < 932) Fuel_L_value = 89; 
+            if (Fuel_L_value < 921) Fuel_L_value = 91; 
+            if (Fuel_L_value < 916) Fuel_L_value = 93; 
+            if (Fuel_L_value < 911) Fuel_L_value = 95; 
+            if (Fuel_L_value < 905) Fuel_L_value = 97; 
+            if (Fuel_L_value < 900) Fuel_L_value = 100; 
+            while (mySerial_L.available() > 0) {
+                mySerial_L.read();
+            }
+        }
+    }
+    
+    // read ttl to serial sensors Right tank
+    if (mySerial_R.available() >= 2) {
+        R_counter++;
+        memset(buffer_R, 0, sizeof(buffer_R));
+        // Read the line until newline character or buffer filled
+        bytesRead_R = mySerial_R.readBytesUntil('\n', buffer_R, sizeof(buffer_R));
+        if (bytesRead_R >= 2) {
+            // (Little Endian)
+            R_counter = (buffer_R[0] | buffer_R[1] << 8);
+
+            if(R_counter < Full_fuel_R_capacitance_value) R_counter = Full_fuel_R_capacitance_value;
+            if(R_counter > Empty_fuel_R_capacitance_value) R_counter = Empty_fuel_R_capacitance_value;
+            Serial.print("Right counter: ");
+            Serial.print(R_counter);
+            Serial.print(" bytesRead: ");
+            Serial.print(bytesRead_R);
+            Serial.print(" buffer: ");
+            int i = 0;
+            // Loop as long as the current character is not 0 AND we are within array bounds
+            while (i < 10 && buffer_R[i] != 0) {
+                Serial.print(buffer_R[i], HEX); // Print the character
+                Serial.print(" ");
+                i++;
+            }
+            Serial.print(" Raw Bytes: ");
+            Serial.print(buffer_R[0], HEX);
+            Serial.print(" ");
+            Serial.print(buffer_R[1], HEX);
+            Serial.print(" Decoded 16-bit Counter: ");
+            Serial.println(R_counter);
+
+            Fuel_R_value = smooth_fuel_readings_R(R_counter);
+            Serial.printf("Raw smoothed sensor R value: %" PRId32 "\n", Fuel_R_value);
+            // Greater than or equal to 1320 is considered empty, so set to 0% 
+            if (Fuel_R_value >= 1320) Fuel_R_value = 0;
+            if (Fuel_R_value < 1320) Fuel_R_value = 2; 
+            if (Fuel_R_value < 1300) Fuel_R_value = 4; 
+            if (Fuel_R_value < 1290) Fuel_R_value = 6; 
+            if (Fuel_R_value < 1278) Fuel_R_value = 8; 
+            if (Fuel_R_value < 1268) Fuel_R_value = 10; 
+            if (Fuel_R_value < 1258) Fuel_R_value = 12; 
+            if (Fuel_R_value < 1248) Fuel_R_value = 14; 
+            if (Fuel_R_value < 1239) Fuel_R_value = 16; 
+            if (Fuel_R_value < 1229) Fuel_R_value = 18; 
+            if (Fuel_R_value < 1219) Fuel_R_value = 20; 
+            if (Fuel_R_value < 1209) Fuel_R_value = 22; 
+            if (Fuel_R_value < 1198) Fuel_R_value = 24; 
+            if (Fuel_R_value < 1191) Fuel_R_value = 26; 
+            if (Fuel_R_value < 1185) Fuel_R_value = 28; 
+            if (Fuel_R_value < 1178) Fuel_R_value = 30;
+            if (Fuel_R_value < 1172) Fuel_R_value = 33; 
+            if (Fuel_R_value < 1163) Fuel_R_value = 35; 
+            if (Fuel_R_value < 1154) Fuel_R_value = 37; 
+            if (Fuel_R_value < 1145) Fuel_R_value = 39; 
+            if (Fuel_R_value < 1137) Fuel_R_value = 42; 
+            if (Fuel_R_value < 1130) Fuel_R_value = 44; 
+            if (Fuel_R_value < 1122) Fuel_R_value = 46; 
+            if (Fuel_R_value < 1115) Fuel_R_value = 48; 
+            if (Fuel_R_value < 1107) Fuel_R_value = 50; 
+            if (Fuel_R_value < 1099) Fuel_R_value = 52; 
+            if (Fuel_R_value < 1090) Fuel_R_value = 54; 
+            if (Fuel_R_value < 1082) Fuel_R_value = 56; 
+            if (Fuel_R_value < 1073) Fuel_R_value = 58; 
+            if (Fuel_R_value < 1064) Fuel_R_value = 60; 
+            if (Fuel_R_value < 1056) Fuel_R_value = 62; 
+            if (Fuel_R_value < 1048) Fuel_R_value = 64; 
+            if (Fuel_R_value < 1040) Fuel_R_value = 66; 
+            if (Fuel_R_value < 1031) Fuel_R_value = 68; 
+            if (Fuel_R_value < 1022) Fuel_R_value = 70; 
+            if (Fuel_R_value < 1013) Fuel_R_value = 72; 
+            if (Fuel_R_value < 1005) Fuel_R_value = 74; 
+            if (Fuel_R_value < 995) Fuel_R_value = 76;
+            if (Fuel_R_value < 985) Fuel_R_value = 79; 
+            if (Fuel_R_value < 975) Fuel_R_value = 81; 
+            if (Fuel_R_value < 965) Fuel_R_value = 83; 
+            if (Fuel_R_value < 952) Fuel_R_value = 85; 
+            if (Fuel_R_value < 943) Fuel_R_value = 87; 
+            if (Fuel_R_value < 932) Fuel_R_value = 89; 
+            if (Fuel_R_value < 921) Fuel_R_value = 91; 
+            if (Fuel_R_value < 916) Fuel_R_value = 93; 
+            if (Fuel_R_value < 911) Fuel_R_value = 95; 
+            if (Fuel_R_value < 905) Fuel_R_value = 97; 
+            if (Fuel_R_value < 900) Fuel_R_value = 100; 
+            while (mySerial_R.available() > 0) {
+                mySerial_R.read();
+            }
+        }
+    }
+
+/*
+    //// Old code for resistive fuel senders, using the ADS7830 ADC directly, without the microcontroller in the fuel tank
+    //Fuel_L_value = smooth_fuel_readings_L(ad7830.readADCsingle(0));
+    //Serial.printf("Raw smoothed sensor L value: %" PRId32 "\n", Fuel_L_value);
+    //if(Fuel_L_value < Full_fuel_capacitance_value) Fuel_L_value = Full_fuel_capacitance_value;
+    //if(Fuel_L_value > Empty_fuel_capacitance_value) Fuel_L_value = Empty_fuel_capacitance_value;
+    //Fuel_L_value = (int32_t)round((Fuel_L_value-13)*1.851);  // normalize the range 0-100
+    //Fuel_L_value = 100 - Fuel_L_value;  // Reverse the range
+    //Serial.printf("Fuel_L_value: %" PRId32 "\n", Fuel_L_value);
+
+    //Fuel_R_value = smooth_fuel_readings_R(ad7830.readADCsingle(1));
+    //Serial.printf("Raw smoothed sensor R value: %" PRId32 "\n", Fuel_R_value);
+    //if(Fuel_R_value < Full_fuel_capacitance_value) Fuel_R_value = Full_fuel_capacitance_value;
+    //if(Fuel_R_value > Empty_fuel_capacitance_value) Fuel_R_value = Empty_fuel_capacitance_value;
+    //Fuel_R_value = (int32_t)round((Fuel_R_value-13)*1.851);  // normalize the range 0-100
+    //Fuel_R_value = 100 - Fuel_R_value;  // Reverse the range
+    //Serial.printf("Fuel_R_value: %" PRId32 "\n", Fuel_R_value);
+*/
+}
+
+static void trim_flap_sensors_timer_cb(lv_timer_t * timer2)
+{
+    LV_UNUSED(timer2);
     // read sensors
-    Fuel_L_value = smooth_fuel_readings_L(ad7830.readADCsingle(0));
-    Serial.printf("Raw smoothed sensor L value: %" PRId32 "\n", Fuel_L_value);
-    if(Fuel_L_value < Full_fuel_resistance_value) Fuel_L_value = Full_fuel_resistance_value;
-    if(Fuel_L_value > Empty_fuel_resistance_value) Fuel_L_value = Empty_fuel_resistance_value;
-    Fuel_L_value = (int32_t)round((Fuel_L_value-13)*1.851);  // normalize the range 0-100
-    Fuel_L_value = 100 -Fuel_L_value;  // Reverse the range
-    Serial.printf("Fuel_L_value: %" PRId32 "\n", Fuel_L_value);
-
-    Fuel_R_value = smooth_fuel_readings_R(ad7830.readADCsingle(1));
-    Serial.printf("Raw smoothed sensor R value: %" PRId32 "\n", Fuel_R_value);
-    if(Fuel_R_value < Full_fuel_resistance_value) Fuel_R_value = Full_fuel_resistance_value;
-    if(Fuel_R_value > Empty_fuel_resistance_value) Fuel_R_value = Empty_fuel_resistance_value;
-    Fuel_R_value = (int32_t)round((Fuel_R_value-13)*1.851);  // normalize the range 0-100
-    Fuel_R_value = 100 - Fuel_R_value;  // Reverse the range
-    Serial.printf("Fuel_R_value: %" PRId32 "\n", Fuel_R_value);
-
-    Flaps_position_value = ad7830.readADCsingle(2);
+    Flaps_position_value = ad7830.readADCsingle(0);
     if(Flaps_position_value < Full_flaps_resistance_value) Flaps_position_value = Full_flaps_resistance_value;
     if(Flaps_position_value > Empty_flaps_resistance_value) Flaps_position_value = Empty_flaps_resistance_value;
     Flaps_position_value = (int32_t)round((Fuel_R_value-13)*1.851);  // normalize the range 0-20
     Flaps_position_value = 20 - Flaps_position_value;  // Reverse the range
     Serial.printf("Flaps_position_value: %" PRId32 "\n", Flaps_position_value);
 
-    elev_trim_value = ad7830.readADCsingle(3);
-    if(elev_trim_value < Full_flaps_resistance_value) elev_trim_value = Full_flaps_resistance_value;
-    if(elev_trim_value > Empty_flaps_resistance_value) elev_trim_value = Empty_flaps_resistance_value;
-    elev_trim_value = (int32_t)round((elev_trim_value-13)*1.851);  // normalize the range 0-20
-    elev_trim_value = 20 - elev_trim_value;  // Reverse the range
-    Serial.printf("elev_trim_value: %" PRId32 "\n", elev_trim_value);
-
-    ailer_trim_value = ad7830.readADCsingle(4);
+    ailer_trim_value = ad7830.readADCsingle(1);
     if(ailer_trim_value < Full_flaps_resistance_value) ailer_trim_value = Full_flaps_resistance_value;
     if(ailer_trim_value > Empty_flaps_resistance_value) ailer_trim_value = Empty_flaps_resistance_value;
     ailer_trim_value = (int32_t)round((ailer_trim_value-13)*1.851);  // normalize the range 0-20
     ailer_trim_value = 20 - ailer_trim_value;  // Reverse the range
     Serial.printf("ailer_trim_value: %" PRId32 "\n", ailer_trim_value);
 
+    elev_trim_value = ad7830.readADCsingle(2);
+    if(elev_trim_value < Full_flaps_resistance_value) elev_trim_value = Full_flaps_resistance_value;
+    if(elev_trim_value > Empty_flaps_resistance_value) elev_trim_value = Empty_flaps_resistance_value;
+    elev_trim_value = (int32_t)round((elev_trim_value-13)*1.851);  // normalize the range 0-20
+    elev_trim_value = 20 - elev_trim_value;  // Reverse the range
+    Serial.printf("elev_trim_value: %" PRId32 "\n", elev_trim_value);
 
 }
 
@@ -180,9 +377,11 @@ void pulseCounter() {
   pulseCount++;
 }
 
-static void flow_sensor_timer_cb(lv_timer_t * timer1) {
+static void flow_sensor_timer_cb(lv_timer_t * timer3) {
+
+    LV_UNUSED(timer3);
+
     detachInterrupt(digitalPinToInterrupt(sensorPin));
-    
     // Formula: Freq / 7.5 = L/min
     flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / 7.5;
     oldTime = millis();
@@ -210,6 +409,12 @@ void setup() {
     #endif
 
     Serial.begin(115200);
+
+    // Left and Right fuel tank Configure 1200 baud, 8N1 
+    Serial.println("Start receiving TTL to serial feeds\n");
+    mySerial_L.begin(1200); 
+    mySerial_R.begin(1200); 
+
     Wire.begin(I2C_SDA, I2C_SCL);
     Serial.println("Adafruit ADS7830 start\n");
     if (!ad7830.begin()) {
@@ -302,7 +507,9 @@ void loop() {
     if (first_time==true) {
         first_time=false;
         // Create timer to run the sensors for fuel, flap, trim
-        lv_timer_create(sensors_timer_cb, 100, NULL);
+        lv_timer_create(fuel_sensors_timer_cb, 100, NULL);
+        // Create timer to run the sensors for fuel, flap, trim
+        lv_timer_create(trim_flap_sensors_timer_cb, 100, NULL);
         // Create the timer to run the flow sensor
         lv_timer_create(flow_sensor_timer_cb, 1000, NULL);
 
